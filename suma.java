71b474241fe911d5ccbf9f58b85f772752240639
Timur Kotsubey <timur.kotsubey@cirvianum.cat>
2020-09-29 16:14:04 +0200

    suma 2 catala
    per fer un conflicte

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Introduce el primer número:");
+System.out.println("Introdueix el primer numero:");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce el segundo número:");
+System.out.println("Introdueix el segon numero:");
 numero2 = reader.nextInt();
 

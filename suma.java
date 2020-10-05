d0ea2245fff4bf17150e234a034ce30af1d6c271
Timur kotsubey <timurkotsubey@gmail.com>
2020-10-05 21:43:19 +0200

    Canvis timur kotsubey

--- suma.java
+++ suma.java
@@ -2,9 +2,9 @@
 int numero1 = 0;
 int numero2 = 0;
 
-System.out.println("Input the first number:");
+System.out.println("Introdueix un numero no tant epic");
 numero1 = reader.nextInt();
 
-System.out.println("Input the second number:");
+System.out.println("introdueix un numero epic:");
 numero2 = reader.nextInt();
 
